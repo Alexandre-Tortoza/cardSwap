008aeed97cd23354107207202f91a2cbcbe846a6
Alexandre Tortoza <85268093+Alexandre-Tortoza@users.noreply.github.com>
2025-06-02 21:34:39 -0300

    [ Feat ] - Home start

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('home', function () {
+    return Inertia::render('Home/Index');
+})->middleware(['auth', 'verified'])->name('home');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
